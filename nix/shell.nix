7630a2aa61b43ce679c8a76905a29164fca487d8
Meng Weng Wong <mengwong@legalese.com>
2025-08-12 21:39:16 +0800

    experiments in static building

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,10 +2,18 @@
   pkgs ? import <nixpkgs> { },
   ...
 }:
+let
+  # Use a static-friendly GHC setup
+  haskellPackages = pkgs.haskell.packages.ghc96.override {
+    overrides = self: super: {
+      # Enable more static-friendly compilation for some base packages
+    };
+  };
+in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.haskell.compiler.ghc98
-    pkgs.haskell.packages.ghc98.haskell-language-server
+    haskellPackages.ghc
+    haskellPackages.haskell-language-server
     pkgs.cabal-install
     pkgs.ghciwatch
     pkgs.zlib
@@ -15,6 +23,48 @@ pkgs.mkShell {
     pkgs.typescript
     pkgs.nixos-anywhere
     pkgs.sqlite
+    pkgs.upx
     pkgs.nodePackages.prettier
+    
+    # Static libraries
+    pkgs.glibc.static  
+    pkgs.gmp6.dev
+    pkgs.libffi.dev
+    pkgs.zlib.dev
+    pkgs.ncurses.dev
+    
+    # Additional tools for static linking
+    pkgs.binutils
+    pkgs.gcc
   ];
+  
+  shellHook = ''
+    # Configure for static linking
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.glibc.static}/lib"
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.gmp6}/lib"
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.libffi}/lib"  
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.zlib}/lib"
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.ncurses}/lib"
+    
+    # Include paths
+    export NIX_CPPFLAGS="$NIX_CPPFLAGS -I${pkgs.gmp6.dev}/include"
+    export NIX_CPPFLAGS="$NIX_CPPFLAGS -I${pkgs.libffi.dev}/include"
+    
+    # Use GNU ld instead of gold for better static support  
+    export NIX_LDFLAGS="$NIX_LDFLAGS -fuse-ld=bfd"
+    
+    # Static linking environment
+    export LDFLAGS="$LDFLAGS -static -pthread"
+    
+    echo "Static linking environment ready"
+    echo "Libraries available:"
+    echo "  GMP: ${pkgs.gmp6}/lib"
+    echo "  LibFFI: ${pkgs.libffi}/lib" 
+    echo "  Glibc static: ${pkgs.glibc.static}/lib"
+    echo ""
+    echo "For static linking, try:"
+    echo "  nix develop -c cabal build all"
+    echo "  or disable static temporarily:"
+    echo "  nix develop -c cabal build all --disable-executable-static"
+  '';
 }
